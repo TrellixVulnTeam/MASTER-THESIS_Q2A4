007441e1df98ebc28881f7516c78fb213dbd6adb
renefiedel <renefiedel@outlook.com>
2021-06-22 16:22:55 +0200

    NEA lightcurves

---
+++ Project/Asteroids NEAs/New NEA's/2003RP8/lc2003RP8.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('2003RP8.csv')
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.scatter(x=df['Phase'], y=df['Mag'], s=10, c='b', marker="+", label='5 Nights of Magnitude')
+ax.scatter(x=df['Phase'], y=df['Curve'], s=10, c='r', label='6th order curve')
+plt.gca().invert_yaxis()
+plt.xlabel("Phase")
+plt.ylabel("Magnitude")
+plt.title('2001 SG276 Light Curve')
+plt.legend(loc='best')
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+
+plt.show()
+fig.savefig('Final_light_curve_2003RP8.png', dpi=fig.dpi)
